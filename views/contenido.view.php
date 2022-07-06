1352509f4491e86dbf5170164d78c6c31e39c354
KevinSotoZ <82842945+KevinSotoZ@users.noreply.github.com>
2022-07-05 23:25:40 -0400

    views

---
+++ views/contenido.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <hr class="border">
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Molestias, recusandae eius sequi illo commodi, 
+                    officiis laboriosam fugiat at rerum quia nesciunt. Qui tempora, 
+                    tempore nesciunt aliquam nisi nemo maiores laborum? 
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Molestias, recusandae eius sequi illo commodi, 
+                    officiis laboriosam fugiat at rerum quia nesciunt. Qui tempora, 
+                    tempore nesciunt aliquam nisi nemo maiores laborum? 
+                </p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
